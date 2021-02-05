59a140af0e47bd95b9578acb429895630cbf1653
nOmeep <37767676+nOmeep@users.noreply.github.com>
2021-02-05 15:27:16 +0300

    с черепашкой не пушил

---
+++ KlickKnight/Assets/Scripts/CharacterActions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterActions : MonoBehaviour
+{
+    private Rigidbody2D _knight;
+    
+    public bool block = false;
+    public bool attack = false;
+    public bool evasion = false;
+    public int health;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _knight = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
