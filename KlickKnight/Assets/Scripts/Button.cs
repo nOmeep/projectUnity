5083984c17984a02acad633ca42a51ccd63e12c9
nOmeep <37767676+nOmeep@users.noreply.github.com>
2021-02-05 13:11:25 +0300

    start working

---
+++ KlickKnight/Assets/Scripts/Button.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private string targetMessage;
+
+    public Color highlightColor = Color.red;
+
+    public void OnMouseEnter()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+
+        if (sprite != null)
+        {
+            sprite.color = highlightColor;
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+
+        if (sprite != null)
+        {
+            sprite.color = Color.white;
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * 1.1f, transform.localScale.y * 1.1f, transform.localScale.z * 1.1f);
+    }
+
+    public void OnMouseUp()
+    {
+        transform.localScale = new Vector3(transform.localScale.x / 1.1f, transform.localScale.y / 1.1f, transform.localScale.z / 1.1f);
+
+        if (targetObject != null)
+        {
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+}
---
+++ KlickKnight/Assets/Scripts/SceneController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AttackIsPressed()
+    {
+        Debug.Log("Attack");
+    }
+
+    public void ShieldIsPressed()
+    {
+        Debug.Log("Shield");
+    }
+
+    public void EvasionIsPressed()
+    {
+        Debug.Log("Evasion");
+    }
+
+    public void Pause()
+    {
+        Debug.Log("Its paused");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
